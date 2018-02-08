2275665f296855c514db015411b7cdd10207ae2e
Hassan <2288115k@student.gla.ac.uk>
2018-02-08 10:39:58 +0000

    Chapter 9 done

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -18,6 +18,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
+#This ensures that the login_required() decorator will redirect any user not logged in to the URL/rango/login/.
+LOGIN_URL = '/rango/login/'
+
 #Media
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
@@ -32,6 +35,7 @@ SECRET_KEY = 'w7dw95t+olflemeds7$7!dchp$@*xi3^l%=8h5itbz*q=bhm-='
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -104,6 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS' : {'min_length' : 6}
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -113,6 +118,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
